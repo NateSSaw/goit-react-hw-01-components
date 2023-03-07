e0da3d4719d13c5986b863d312d4bfe061a0d141
Nataliia Semenova <natasemenova1806@gmail.com>
2023-03-07 23:11:10 +0200

    fixed title

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,7 +3,7 @@ import css from './Statistics.module.css';
 
 export default function Statistics({ title, stats }) {
     return <section className={css.statistics}>
-        {title && <h2 className={css.title}>Upload stats</h2>}
+        {title && <h2 className={css.title}>{title}</h2>}
 
         <ul className={css["stat-list"]}>
             
